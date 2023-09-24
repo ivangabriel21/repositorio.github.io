6d1d6a09e33be503066831a33e299d8ba64b8da7
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-09-24 17:47:02 -0400

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,11 @@
+apt update
+apt innstall wget
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+read -p "Enter "
+sudo apt install docker-ce
+sudo systemctl status docker
+read -p "enter"
