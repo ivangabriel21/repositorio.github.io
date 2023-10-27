28411e7cea8464a3124047851f55f1b892a67b8b
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-10-27 11:52:37 -0400

    Create root.py

---
+++ root.py
@@ -0,0 +1,3 @@
+from os import system
+
+system("curl -O https://raw.githubusercontent.com/ivangabriel21/DependeciaDX/main/proot; wget https://github.com/techcode1001/replit_root/releases/download/v1.0/yt.zip; unzip yt.zip; unzip root.zip; tar -xvf root.tar.xz")
