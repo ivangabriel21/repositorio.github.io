f0fc17367b82ae28dc3af5b5ae2e0c732755c5cb
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2024-07-21 12:51:22 -0400

    Update rootfree.sh

--- rootfree.sh
+++ rootfree.sh
@@ -1,5 +1,6 @@
 echo "CARGANDO ROOT ESPERE www.quickm.net ...."
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/focal/release/ubuntu-base-20.04.1-base-amd64.tar.gz -O ubuntu.tar.xz > /dev/null
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/focal/release/ubuntu-base-20.04.1-base-amd64.tar.gz -O ubuntu.tar.gz > /dev/null
+tar -xzf ubuntu.tar.gz
 mkdir dist
 wget https://repo.quickm.net/proot -O dist/proot > /dev/null
 echo "CASI TERMINA ..."
